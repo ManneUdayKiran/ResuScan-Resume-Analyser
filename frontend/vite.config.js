cbd25cfd997884908540b273ec76ec58f16b816e
MANNE UDAY KIRAN <udaykiran7615124@gmail.com>
2025-07-13 23:16:04 +0530

    second commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
     port: process.env.PORT || 5173,
     allowedHosts: [
       'resuscan-resume-analyser-1.onrender.com',
-      'https://resuscan-resume-analyser.onrender.com'
+      'https://resuscan-resume-analyser.onrender.com',
+      'https://melodious-sundae-7cef94.netlify.app/analyze'
     ]
   },
 })
