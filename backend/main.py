fd9e9bfbbfb1fa0879f00acf464708e8c498dde6
MANNE UDAY KIRAN <udaykiran7615124@gmail.com>
2025-12-25 19:58:06 +0530

    chore: sync README changes

--- backend/main.py
+++ backend/main.py
@@ -43,7 +43,6 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
-        "https://resuscan-resume-analyser.onrender.com",
         "*",
     ],
     allow_credentials=True,
